727c75d13ec3714d098f9f97c6e612f9cd2175c0
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-12-24 19:05:40 +0530

    Update BarChart.js

--- src/DashboardADmin/BarChart.js
+++ src/DashboardADmin/BarChart.js
@@ -63,7 +63,7 @@ const fetchCustomerOrders = async () => {
 
 try {
 
-const response = await fetch("https://naturalbuti.onrender.com/fetchCutomerOrder");
+const response = await fetch("https://jsonsoftech.onrender.com/fetchCutomerOrder");
 const data = await response.json();
 
 // Month-wise 
